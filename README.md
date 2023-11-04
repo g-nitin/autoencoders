0389506121579a1df731a028ba68cc68d518f325
niting <niting1209@gmail.com>
2023-11-04 14:48:21 -0400

    Adding data builder

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 The code (running `main.py`) will generate a `results` folder which has sub-folders for each new model test.
 To avoid pushing large amounts of data (in the `results` sub-folder), the sub-folder has been ignored.
+
+The `data` folder contains the `data_builder.py` file, which uses the [google-landmark](https://github.com/cvdfoundation/google-landmark) dataset to fetch images of size 600 by 800.
+To get the data, just run the `data_build.py` file, which will create a sub-folder (`google-landmark`) in `data`.
+That sub-folder contains the `train` and `test` images in those sub-folders.
---
+++ data/data_builder.py
@@ -0,0 +1,141 @@
+import subprocess
+import os
+import shutil
+from torchvision.io import read_image
+
+
+def main():
+    main_dir = os.getcwd()
+
+    if not os.path.exists('google-landmark'):
+        os.makedirs('google-landmark')
+        subprocess.run(['git', 'clone', 'https://github.com/cvdfoundation/google-landmark.git'])
+        print('google-landmark directory created\n')
+
+    train_path = os.path.join('google-landmark', 'train')
+    os.makedirs(train_path, exist_ok=True)
+    test_path = os.path.join('google-landmark', 'test')
+    os.makedirs(test_path, exist_ok=True)
+    print('Train and Test directories created under google-landmark\n')
+
+    script_path = os.path.join('..', 'download-dataset.sh')  # Path for the download script
+
+    os.chdir(train_path)
+    subprocess.run(['bash', script_path, 'train', '000'])
+    print('Downloaded training images\n')
+    os.chdir(main_dir)  # Go back
+
+    os.chdir(test_path)
+    subprocess.run(['bash', script_path, 'test', '00'])
+    print('Downloaded test images\n')
+    os.chdir(main_dir)
+
+    # Since the train and test folders contain images within many sub-folders,
+    # Get all images and move them to a single level folder...
+    train_path_new = os.path.join('google-landmark', 'train_new')
+    os.makedirs(train_path_new, exist_ok=True)
+    test_path_new = os.path.join('google-landmark', 'test_new')
+    os.makedirs(test_path_new, exist_ok=True)
+
+    print(f"Relocated {relocate_jpg_files(train_path, train_path_new)} training files.")
+    print(f"Relocated {relocate_jpg_files(test_path, test_path_new)} testing files.\n")
+
+    # Remove the original folders
+    subprocess.run(['rm', '-rf', train_path])
+    print(f'Removed {train_path}')
+    subprocess.run(['rm', '-rf', test_path])
+    print(f'Removed {test_path}\n')
+
+    # Keep only those images which have a 600*800 size...
+    # Get a list of paths for each images
+    train = [os.path.join(train_path_new, file) for file in os.listdir(train_path_new)]
+    test = [os.path.join(test_path_new, file) for file in os.listdir(test_path_new)]
+
+    # Create dictionaries to store the image paths and their corresponding shapes
+    train_dict = get_shape_paths(train, desired_shape=(3, 600, 800))
+    test_dict = get_shape_paths(test, desired_shape=(3, 600, 800))
+
+    # Move these files to the original folders
+    os.makedirs(train_path)
+    os.makedirs(test_path)
+    print(f"Relocated {relocate_from_dict(train_dict, train_path)} training images of size 600 * 800")
+    print(f"Relocated {relocate_from_dict(test_dict, test_path)} testing images of size 600 * 800\n")
+
+    # Remove the new folders
+    subprocess.run(['rm', '-rf', train_path_new])
+    print(f'Removed {train_path_new}')
+    subprocess.run(['rm', '-rf', test_path_new])
+    print(f'Removed {test_path_new}')
+
+
+def relocate_from_dict(img_dict, new_path, move=True):
+    """
+    Relocate files given in the keys of `img_dict` to the `new_path`.
+    :param img_dict: dict for the images (as keys)
+    :param new_path: str for the new path for those images
+    :param move: bool for whether to move (True) or copy (False)
+    :return: int indicating the number of files moved
+    """
+    num = 0
+    for path in img_dict.keys():
+        file = os.path.split(path)[1]
+
+        if move:
+            shutil.move(path, os.path.join(new_path, file))
+        else:
+            shutil.copy(path, os.path.join(new_path, file))
+
+        num += 1
+    return num
+
+
+def relocate_jpg_files(src, dst, move=True):
+    """
+    Move or Cpy images from the `src` directory to the `dst` directory.
+    This is done recursively, meaning that the function will move (or copy) all files in all subdirectories of `src`.
+    :param src: The source directory
+    :param dst: The final directory
+    :param move: Whether to move files (True) or copy them (False)
+    :return: The number of files moved
+    """
+    num = 0
+    for root, dirs, files in os.walk(src):
+        for file in files:
+            if file.lower().endswith('.jpg'):
+                src_file = os.path.join(root, file)
+                dst_file = os.path.join(dst, file)
+
+                if move:
+                    shutil.move(src_file, dst_file)
+                else:
+                    shutil.copy(src_file, dst_file)
+                num += 1
+
+    return num
+
+
+def get_shape_paths(paths, desired_shape):
+    """
+    Given a list of image paths, return a dictionary of the path (key) and its shape (value).
+    Only return those images which have the `desired_shape`.
+    Using shape() from torchvision.io.read_image
+    :param paths: A list of image paths
+    :param desired_shape: A tuple for the desired shape
+    :return: dict of paths to their shapes
+    """
+    paths_dict = {}
+
+    for path in paths:
+        # Read the image and get its shape
+        shape = read_image(path).shape
+
+        # Check if the shape matches the desired shape
+        if shape == desired_shape:
+            # Store the shape in the dictionary with the image path as the key
+            paths_dict[path] = shape
+
+    return paths_dict
+
+
+if __name__ == '__main__':
+    main()
