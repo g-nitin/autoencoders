507a3e2b6a3347a20c5a0b2f0a2a964ab9dfcdc2
niting <niting1209@gmail.com>
2023-11-22 18:03:56 -0500

    Updates...

--- main.py
+++ main.py
@@ -6,48 +6,54 @@ from os import makedirs
 from torch.cuda import is_available
 import sys
 
-use_gpu = sys.argv[1]
 
-# If you have to GPUs and the GPUs are not available
-if use_gpu == 'True'  and not is_available():
-    print('GPUs not available')	
-    exit()
+def main():
+    use_gpu = sys.argv[1]
 
-architecture = 7
-learning_rate = 0.001
-perc = 1
-prefix = "3"
+    # If you have to GPUs and the GPUs are not available
+    if use_gpu == 'True'  and not is_available():
+        print('GPUs not available')
+        exit()
 
-makedirs('results', exist_ok=True)
-folder_name = join('results', f"{architecture}_{learning_rate}_{prefix}")
-if exists(folder_name):
-    print(f'This results folder already exists: {folder_name}'
-          f'\nTry changing the name to avoid overriding')
-    exit()
-makedirs(folder_name)
+    architecture = 7
+    learning_rate = 0.001
+    perc = 1
+    prefix = "3"
 
-print()
-t = time.time()
-model_file = train_model(color_dir=join('data', 'google-landmark', 'train'),
-                         perc=perc,
-                         folder_name=folder_name,
-                         file_name=f"cnn_{prefix}",
-                         architecture=architecture,
-                         gray_dir=None,
-                         epochs=30,
-                         learning_rate=learning_rate,
-                         model=None)
+    makedirs('results', exist_ok=True)
+    folder_name = join('results', f"{architecture}_{learning_rate}_{prefix}")
+    if exists(folder_name):
+        print(f'This results folder already exists: {folder_name}'
+              f'\nTry changing the name to avoid overriding')
+        exit()
+    makedirs(folder_name)
 
-t = time.time() - t
-print(f"Training ran for {time.strftime('%H:%M:%S', time.gmtime(t))}")
-print(f"\n{model_file} created.")
+    print()
+    t = time.time()
+    model_file = train_model(color_dir=join('data', 'google-landmark', 'train'),
+                             perc=perc,
+                             folder_name=folder_name,
+                             file_name=f"cnn_{prefix}",
+                             architecture=architecture,
+                             gray_dir=None,
+                             epochs=30,
+                             learning_rate=learning_rate,
+                             model=None)
 
-print("\nTesting")
-test_model(model_file=model_file,
-           color_dir=join('data', 'google-landmark', 'test'),
-           perc=perc,
-           folder_name=folder_name,
-           file_name="cnn_testing",
-           architecture=architecture,
-           gray_dir=None,
-           show_images=True)
+    t = time.time() - t
+    print(f"Training ran for {time.strftime('%H:%M:%S', time.gmtime(t))}")
+    print(f"\n{model_file} created.")
+
+    print("\nTesting")
+    test_model(model_file=model_file,
+               color_dir=join('data', 'google-landmark', 'test'),
+               perc=perc,
+               folder_name=folder_name,
+               file_name="cnn_testing",
+               architecture=architecture,
+               gray_dir=None,
+               show_images=True)
+
+
+if __name__ == "__main__":
+    main()
